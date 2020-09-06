1b6042e3ec5a112da41b5f4d5aede0f4241e8982
Maxime Mion <Maxime@Maximes-Mini.fritz.box>
2020-09-06 22:33:16 +0200

    created package.json, added octave, added oscdummy

--- synth.js
+++ synth.js
@@ -1,44 +1,56 @@
-var notes = ["C", "D", "E", "F", "G", "A", "B"]
-const synth = new Tone.PolySynth().toDestination();
+//Master Synth Engine
+var notes = ["C", "D", "E", "F", "G", "A", "B"]//available note
+const synth = new Tone.PolySynth().toDestination();//creates new synth instrument
 var html = ""
 
-for (var octave = 0; octave < 2 ; octave++)
+//set max amount of octaves
+for (var octave = 0; octave < 3 ; octave++)
 {
     for (var i = 0; i < notes.length; i++) {
         var note = notes[i];
         var hasSharp = true;
-    
+        
+        //eliminate E# and B# keys
         if (note === "E" || note === "B"){
             hasSharp = false;
         }
-    
+        //create white key class within keyboard container
         html += `<div class="whitenote"
             onmousedown = "noteDown(this, false)"
             onmouseleave = "noteUp(this, false)"
-            data-note="${note + (octave + 4)}">`;
-    
+            data-note="${note + (octave + 3)}">`;
+        //create black key class within keyboard container
         if (hasSharp){
             html += `<div class="blacknote"
             onmousedown = "noteDown(this, true)"
             onmouseleave = "noteUp(this, true)"
-            data-note="${note + "#" + (octave + 4)}"></div>`;
+            data-note="${note + "#" + (octave + 3)}"></div>`;
         }
         
         html += `</div>`
     }
 }
 
-
-document.getElementById("container").innerHTML = html;
+document.getElementById("keyboard").innerHTML = html;
 
 function noteDown(elem, isSharp){
     var note = elem.dataset.note;
     console.log("user triggered", note);
     elem.style.background = isSharp ? "rgb(78, 78, 78)" : "#ccc"
-    synth.triggerAttackRelease(note, "16n");
+    synth.triggerAttackRelease(note);
     event.stopPropagation();
 }
 
 function noteUp(elem, isSharp){
-    elem.style.background = isSharp ? "#777" : "#fff"
+    elem.style.background = isSharp ? "rgb(158, 158, 158)" : "none"
+    synth.triggerRelease(note)
 }
+
+/* Chain
+const osc = new Tone.Oscillator()
+const env
+const filter
+const distortion
+
+synth.chain(osc, env, filter, distortion, Tone.Destination)
+*/
