8a2534e850b5e16227f820f0fe408ec1768393bb
Maxime Mion <Maxime@Maximes-Mini.fritz.box>
2020-09-06 19:47:43 +0200

    edit style-added key colouring for noteup/notedown

--- synth.js
+++ synth.js
@@ -13,12 +13,14 @@ for (var octave = 0; octave < 2 ; octave++)
         }
     
         html += `<div class="whitenote"
-            onmousedown = "noteDown(this)"
+            onmousedown = "noteDown(this, false)"
+            onmouseleave = "noteUp(this, false)"
             data-note="${note + (octave + 4)}">`;
     
         if (hasSharp){
             html += `<div class="blacknote"
-            onmousedown = "noteDown(this)"
+            onmousedown = "noteDown(this, true)"
+            onmouseleave = "noteUp(this, true)"
             data-note="${note + "#" + (octave + 4)}"></div>`;
         }
         
@@ -29,9 +31,14 @@ for (var octave = 0; octave < 2 ; octave++)
 
 document.getElementById("container").innerHTML = html;
 
-function noteDown(elem){
+function noteDown(elem, isSharp){
     var note = elem.dataset.note;
     console.log("user triggered", note);
+    elem.style.background = isSharp ? "rgb(78, 78, 78)" : "#ccc"
     synth.triggerAttackRelease(note, "16n");
     event.stopPropagation();
+}
+
+function noteUp(elem, isSharp){
+    elem.style.background = isSharp ? "#777" : "#fff"
 }
