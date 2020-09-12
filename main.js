6618d2ae336a6611db3936f38797cc120e936cc3
Maxime Mion <Maxime@Maximes-Mini.fritz.box>
2020-09-12 23:10:45 +0200

    started on oscilloscope need to get mediastream

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ document.getElementById("keyboard").innerHTML = html;
 function noteDown(elem, isSharp){
     var note = elem.dataset.note;
     console.log("user triggered", note);
-    elem.style.background = isSharp ? "rgb(78, 78, 78)" : "#ccc"
+    elem.style.background = isSharp ? "rgb(78, 78, 78)" : "#ccc";
     synth.triggerAttackRelease(note);
     event.stopPropagation();
 }
@@ -92,7 +92,7 @@ volumeSlider.addEventListener("mousemove", function(){
     }
 })
 
-var envelopeSlider = document.getElementById("envelope-sliders")
+var envelopeSlider = document.getElementById("envelope-sliders")        
 envelopeSlider.oninput = function(event){
     switch(event.target.id){
         case "attack-slider":
@@ -109,3 +109,49 @@ envelopeSlider.oninput = function(event){
         break;
     }
 }
+
+
+var canvas = document.querySelector('.oscilloscope');
+var audioCtx = new window.AudioContext();
+var analyser = audioCtx.createAnalyser();
+var stream = navigator.getUserMedia();
+var source = audioCtx.createMediaStreamSource(stream);
+source.connect(analyser);
+analyser.fftSize = 2048;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+var canvasCtx = canvas.getContext("2d");
+canvasCtx.clearRect(0, 0, 70, 10);
+var width = Number(canvas.width)
+var height = Number(canvas.height)
+
+function draw(){
+    var drawVisual = requestAnimationFrame(draw);
+    analyser.getByteTimeDomainData(dataArray);
+    canvasCtx.fillStyle = 'rgb(229, 229, 229)';
+    canvasCtx.fillRect(0, 0, width, height);
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = '#18c947';
+    canvasCtx.beginPath();
+    var sliceWidth = 70 * 1.0 / bufferLength;
+    var x = 0;
+
+    for(var i = 0; i < bufferLength; i++) {
+   
+        var v = dataArray[i] / 128.0;
+        var y = v * height/2;
+
+        if(i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+
+};
+draw();
