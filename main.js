d05d5e117f5d92a48dbdcdd2469a40b632709a12
Maxime Mion <Maxime@Maximes-Mini.fritz.box>
2020-09-13 20:28:11 +0200

    imported nexus ui (fuck WebAudioAPI)

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ for (var octave = 0; octave < 2 ; octave++)
             html += `<div class="blacknote"
             onmousedown = "noteDown(this, true)"
             onmouseleave = "noteUp(this, true)"
+            ontouchstart = "noteDown(this, false)"
+            ontouchend = "noteUp(this, false)"
             data-note="${note + "#" + (octave + 3)}"></div>`;
         }
         
@@ -37,7 +39,6 @@ document.getElementById("keyboard").innerHTML = html;
 
 function noteDown(elem, isSharp){
     var note = elem.dataset.note;
-    console.log("user triggered", note);
     elem.style.background = isSharp ? "rgb(78, 78, 78)" : "#ccc";
     synth.triggerAttackRelease(note);
     event.stopPropagation();
@@ -110,48 +111,8 @@ envelopeSlider.oninput = function(event){
     }
 }
 
+var oscilloscope = new Nexus.Oscilloscope('.oscilloscope-container',{
+    'size': [300,150]
+})
+oscilloscope.connect(Tone.Destination);
 
-var canvas = document.querySelector('.oscilloscope');
-var audioCtx = new window.AudioContext();
-var analyser = audioCtx.createAnalyser();
-var stream = navigator.getUserMedia();
-var source = audioCtx.createMediaStreamSource(stream);
-source.connect(analyser);
-analyser.fftSize = 2048;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-var canvasCtx = canvas.getContext("2d");
-canvasCtx.clearRect(0, 0, 70, 10);
-var width = Number(canvas.width)
-var height = Number(canvas.height)
-
-function draw(){
-    var drawVisual = requestAnimationFrame(draw);
-    analyser.getByteTimeDomainData(dataArray);
-    canvasCtx.fillStyle = 'rgb(229, 229, 229)';
-    canvasCtx.fillRect(0, 0, width, height);
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = '#18c947';
-    canvasCtx.beginPath();
-    var sliceWidth = 70 * 1.0 / bufferLength;
-    var x = 0;
-
-    for(var i = 0; i < bufferLength; i++) {
-   
-        var v = dataArray[i] / 128.0;
-        var y = v * height/2;
-
-        if(i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
-
-        x += sliceWidth;
-    }
-
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
-
-};
-draw();
